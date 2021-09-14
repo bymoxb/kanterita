6f4aec0483cc0d9d6bdd00e10acb936912247fd8
Luis Fernando <moxb@outlook.es>
2021-09-13 21:46:39 -0500

    build(back): init backend project

---
+++ back/src/main/java/com/kruger/kanterita/KanteritaApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.kanterita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanteritaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanteritaApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/kruger/kanterita/KanteritaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.kanterita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanteritaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
